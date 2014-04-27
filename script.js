e1ddb9f724451a4be44a96f75475844b3d69d42c
Mige <mige313@gmail.com>
2014-04-27 13:43:19 -0400

    cleanup

--- script.js
+++ script.js
@@ -2,14 +2,14 @@ window.onload = function date() {
 	var d = new Date();
 
 	// assign month
-	var monthA = new Array("January", "February", "March", "April", "May", "June",
+	var month = new Array("January", "February", "March", "April", "May", "June",
 		"July", "August", "September", "October", "November", "December");
-	var month = monthA[d.getMonth()];
+	month = month[d.getMonth()];
 
-	// assign day of the week
-	var weekday = new Array("Sunday", "Monday", "Tuesday", "Wednesday",
+	// assign day
+	var day = new Array("Sunday", "Monday", "Tuesday", "Wednesday",
 		"Thursday", "Friday", "Saturday");
-	var day = weekday[d.getDay()];
+	day = day[d.getDay()];
 
 	// convert hours
 	var hours = d.getHours();
@@ -18,23 +18,22 @@ window.onload = function date() {
 	if (hours > 12) {
 	    hours -= 12;
 	    m = "PM"
-	} else if (hours === 0) {
-	   hours = 12;
-	}
+	} else if (hours === 0)
+		hours = 12;
 
 	// convert minutes
 	var minutes = d.getMinutes();
-	if (minutes < 10)
+	if (minutes < 10) 
 		minutes = "0" + d.getMinutes();
 
-	var x = document.getElementById("date");
-	x.innerHTML = hours + ":" + minutes + " " + m + "<br>";
-	x.innerHTML += day + ", " + month + " " + d.getDate();
+	// output date
+	document.getElementById("date").innerHTML += hours + ":" + minutes + " " + m +
+	 "<br>" + day + ", " + month + " " + d.getDate();
 
 	// change background colour based on time
 	var elem = document.getElementById("html");
 	if (d.getHours() > 21 && d.getHours() < 7)
-		elem.setAttribute("style", "background-color: #BBBBBB;");
+		elem.setAttribute("style", "background-color:#BBBBBB;");
 	else
-		elem.setAttribute("style", "background-color: #EEEEEE;");
+		elem.setAttribute("style", "background-color:#EEEEEE;");
 }
