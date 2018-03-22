d58acd6aaf41919de028f52623d2b6f93ac6865b
Miguel <mige.d@outlook.com>
2018-03-22 03:21:27 -0400

    use moment.js

--- script.js
+++ script.js
@@ -1,34 +1,9 @@
 window.onload = function date() {
 	var d = new Date();
 
-	// assign month
-	var month = new Array("January", "February", "March", "April", "May", "June",
-		"July", "August", "September", "October", "November", "December");
-	month = month[d.getMonth()];
-
-	// assign day
-	var day = new Array("Sunday", "Monday", "Tuesday", "Wednesday",
-		"Thursday", "Friday", "Saturday");
-	day = day[d.getDay()];
-
-	// convert hours
-	var hours = d.getHours();
-	var m = "AM";
-
-	if (hours > 12) {
-	    hours -= 12;
-	    m = "PM"
-	} else if (hours === 0)
-		hours = 12;
-
-	// convert minutes
-	var minutes = d.getMinutes();
-	if (minutes < 10) 
-		minutes = "0" + d.getMinutes();
-
 	// output date
-	document.getElementById("date").innerHTML += hours + ":" + minutes + " " + m +
-	 "<br>" + day + ", " + month + " " + d.getDate();
+	document.getElementById("date").innerHTML += moment().format('h:mm A') + "<br>";
+	document.getElementById("date").innerHTML += moment().format('dddd, MMM D');
 
 	// change background colour based on time
 	var elem = document.getElementById("html");
